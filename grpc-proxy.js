d0591a3934917d210821ab4eaefad1ab6fb5a9a6
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2025-01-26 19:21:19 +0530

    gRPC proxy ready for deployment

---
+++ grpc-proxy.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const grpcWeb = require('@grpc-web/middleware');
+
+const app = express();
+
+// Specify the gRPC server address
+const grpcServerAddress = 'http://localhost:50051'; // Update with your gRPC server address
+
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+  });
+
+//app.use(cors({ exposedHeaders: ['grpc-status', 'grpc-message'] }));
+app.use(cors({
+    origin: '*', // Allow all origins for testing
+    exposedHeaders: ['grpc-status', 'grpc-message'],
+  }));
+  
+app.use(grpcWeb(grpcServerAddress)); // Pass the gRPC server address
+
+
+app.listen(8080, () => {
+  console.log('gRPC-Web Proxy is running on port 8080');
+});
